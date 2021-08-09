ba2192bedf4b5b76e864fb9588b3b88a20bd648c
Simonbelete <simonbelete@gmail.com>
2021-08-09 08:32:38 +0300

    save me

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 camera.position.setZ(30);
 
-renderer.render( scene, camera );
-
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshStandardMaterial( { color: 0xa5825b } );
-const torus = new THREE.Mesh( geometry, material );
-scene.add( torus );
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set( 5, 5, 5 );
-// scene.add( pointLight );
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add( pointLight, ambientLight ); 
-
 const animate = () => {
   requestAnimationFrame( animate );
-
-  torus.rotation.x += 0.01
-  torus.rotation.y += 0.005
-  torus.rotation.z += 0.01
 
   renderer.render( scene, camera );
 }
