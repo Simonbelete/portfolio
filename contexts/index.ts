9503f2233ca5f8784788aa8836986ced4b157923
Simonbelete <simonbelete@gmail.com>
2021-12-17 21:50:10 +0300

    reactjs typescript setup with tailwindcss

--- contexts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TrackerContext } from "./trackerContext";
-
-export default null;
--- contexts/trackerContext.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const TrackerContext = React.createContext<any>(null);
-
-export default TrackerContext;
