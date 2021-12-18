6716e5b331e1c60e940a2ef72019eee5fbf8faeb
Simonbelete <simonbelete@gmail.com>
2021-12-18 21:43:27 +0300

    loading bar animation example

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -4,9 +4,11 @@ const LoadingBar: React.FC = (): ReactElement => {
   return (
     <div className="border border-[#979696] rounded w-36 h-4 py-[1px] px-[5px] flex gap-[4px] ">
       <div
-        className=" h-full w-[11px] "
+        className=" h-full w-[11px]"
         style={{
-          // background: "rgb(46,63,210)",
+          animation: "myfirst 5s 2",
+          position: "relative",
+          animationDirection: "alternate",
           background:
             "linear-gradient(to top, #4866f6 40%, #bec5ff 70%, #4866f6)",
         }}
@@ -14,7 +16,6 @@ const LoadingBar: React.FC = (): ReactElement => {
       <div
         className="h-full w-[11px] "
         style={{
-          // background: "rgb(46,63,210)",
           background:
             "linear-gradient(to top, #4866f6 40%, #bec5ff 60%, #4866f6)",
         }}
@@ -22,7 +23,6 @@ const LoadingBar: React.FC = (): ReactElement => {
       <div
         className="h-full w-[11px]"
         style={{
-          // background: "rgb(46,63,210)",
           background:
             "linear-gradient(to top, #4866f6 20%, #bec5ff 50%, #4866f6)",
         }}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        leftToRight: {
+          '0%': { left: '0px'},
+          '25%': { left: '50px'},
+          '50%': { left: '100px'},
+          '75%': { left: '150px'},
+          '10%': { left: '200px'}
+        }
+      }
+    },
   },
   plugins: [],
 }
