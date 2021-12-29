665f2590b874f0db1e11aa2de02d355e45ad39c6
Simonbelete <simonbelete@gmail.com>
2021-12-29 13:44:01 +0300

    github pages deploument

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "UA-12341234-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
