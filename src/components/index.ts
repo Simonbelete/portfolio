56b3bccfef06a0024c27084ae636b9e2492327f7
Simonbelete <simonbelete@gmail.com>
2022-01-19 18:34:28 +0300

    deleting background image container

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as LoadingBar } from "./LoadingBar";
 export { default as Folder } from "./Folder";
-export { default as BackgroundImageWrapper } from "./BackgroundImageWrapper";
 export { default as FontWrapper } from "./FontWrapper";
 export { default as StartButton } from "./StartButton";
 export { default as StartBar } from "./StartBar";
