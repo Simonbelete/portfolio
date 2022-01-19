a99c26449292c09666d30b1a6f12fdaede1ef8d4
Simonbelete <simonbelete@gmail.com>
2022-01-19 11:20:38 +0300

    power off screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React, { ReactElement, useEffect, useState } from "react";
 import { useGaTracker } from "hooks";
-import { UserPictureListSelector } from "components";
+import { PowerOffScreen } from "components/screens";
 
 const App: React.FC = (): ReactElement => {
   // Init ga tracker
   useGaTracker();
 
-  return (
-    <div className="w-screen h-screen overflow-scroll">
-      <UserPictureListSelector />
-    </div>
-  );
+  return <PowerOffScreen />;
 };
 
 export default App;
--- src/components/screens/index.tsx
+++ src/components/screens/index.tsx
@@ -1,2 +1,3 @@
 export { default as LockScreen } from "./LockScreen";
 export { default as WelcomeScreen } from "./WelcomeScreen";
+export { default as PowerOffScreen } from "./PoweOffScreen";
