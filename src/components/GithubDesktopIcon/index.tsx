e3dfa143dfe4e441bdef36b18635def4c2fae8da
Simonbelete <simonbelete@gmail.com>
2022-01-20 17:19:27 +0300

    deleting github desktop icon component

--- src/components/GithubDesktopIcon/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from "react";
-import { DesktopIcon } from "components";
-
-const GithubdesktopIcon: React.FC = (): ReactElement => {
-  return <DesktopIcon iconUrl="/images/github-logo.png">Github</DesktopIcon>;
-};
-
-export default GithubdesktopIcon;
